54525d8a80029fb615a73b30562bb4c34133351a
Fabricio Cabral <fabriciofx@gmail.com>
2018-04-19 08:31:11 -0300

    SmartMatch tests updated

--- src/test/java/com/github/fabriciofx/rps/match/SmartMatchTest.java
+++ src/test/java/com/github/fabriciofx/rps/match/SmartMatchTest.java
@@ -23,15 +23,11 @@
  */
 package com.github.fabriciofx.rps.match;
 
-import com.github.fabriciofx.rps.Match;
 import com.github.fabriciofx.rps.Moves;
-import com.github.fabriciofx.rps.match.SmartMatch;
 import com.github.fabriciofx.rps.player.FakePlayer;
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
 import org.junit.Test;
-
-import static org.junit.Assert.assertEquals;
 
 public final class SmartMatchTest {
     @Test
@@ -92,61 +88,71 @@ public final class SmartMatchTest {
 
     @Test
     public void paperWinsRock() {
-        final Match match = new SmartMatch(
-            new FakePlayer(1, Moves.PAPER),
-            new FakePlayer(2, Moves.ROCK)
-        );
-        assertEquals(
-            "Player 1 wins!! Player 1 played Paper and Player 2 played Rock\n",
-            match.result().toString()
+        MatcherAssert.assertThat(
+            "Can't paper win with two players playing paper and rock",
+            new SmartMatch(
+                new FakePlayer(1, Moves.PAPER),
+                new FakePlayer(2, Moves.ROCK)
+            ).result().toString(),
+            Matchers.equalTo(
+                "Player 1 wins!! Player 1 played Paper and Player 2 played Rock\n"
+            )
         );
     }
 
     @Test
     public void scissorsWinsPaper() {
-        final Match match = new SmartMatch(
-            new FakePlayer(1, Moves.SCISSORS),
-            new FakePlayer(2, Moves.PAPER)
-        );
-        assertEquals(
-            "Player 1 wins!! Player 1 played Scissors and Player 2 played Paper\n",
-            match.result().toString()
+        MatcherAssert.assertThat(
+            "Can't scissors win with two players playing scissors and paper",
+            new SmartMatch(
+                new FakePlayer(1, Moves.SCISSORS),
+                new FakePlayer(2, Moves.PAPER)
+            ).result().toString(),
+            Matchers.equalTo(
+                "Player 1 wins!! Player 1 played Scissors and Player 2 played Paper\n"
+            )
         );
     }
 
     @Test
     public void rockLoosesPaper() {
-        final Match match = new SmartMatch(
-            new FakePlayer(1, Moves.ROCK),
-            new FakePlayer(2, Moves.PAPER)
-        );
-        assertEquals(
-            "Player 2 wins!! Player 2 played Paper and Player 1 played Rock\n",
-            match.result().toString()
+        MatcherAssert.assertThat(
+            "Can't paper win with two players playing rock and paper",
+            new SmartMatch(
+                new FakePlayer(1, Moves.ROCK),
+                new FakePlayer(2, Moves.PAPER)
+            ).result().toString(),
+            Matchers.equalTo(
+                "Player 2 wins!! Player 2 played Paper and Player 1 played Rock\n"
+            )
         );
     }
 
     @Test
     public void paperLoosesScissors() {
-        final Match match = new SmartMatch(
-            new FakePlayer(1, Moves.PAPER),
-            new FakePlayer(2, Moves.SCISSORS)
-        );
-        assertEquals(
-            "Player 2 wins!! Player 2 played Scissors and Player 1 played Paper\n",
-            match.result().toString()
+        MatcherAssert.assertThat(
+            "Can't scissors win with two players playing papers and scissors",
+            new SmartMatch(
+                new FakePlayer(1, Moves.PAPER),
+                new FakePlayer(2, Moves.SCISSORS)
+            ).result().toString(),
+            Matchers.equalTo(
+                "Player 2 wins!! Player 2 played Scissors and Player 1 played Paper\n"
+            )
         );
     }
 
     @Test
     public void scissorsLoosesRock() {
-        final Match match = new SmartMatch(
-            new FakePlayer(1, Moves.SCISSORS),
-            new FakePlayer(2, Moves.ROCK)
-        );
-        assertEquals(
-            "Player 2 wins!! Player 2 played Rock and Player 1 played Scissors\n",
-            match.result().toString()
+        MatcherAssert.assertThat(
+            "Can't rock win with two players playing scissors and rock",
+            new SmartMatch(
+                new FakePlayer(1, Moves.SCISSORS),
+                new FakePlayer(2, Moves.ROCK)
+            ).result().toString(),
+            Matchers.equalTo(
+                "Player 2 wins!! Player 2 played Rock and Player 1 played Scissors\n"
+            )
         );
     }
 }
