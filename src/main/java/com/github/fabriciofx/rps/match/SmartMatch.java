8a21298a5479a7454c4a6c3b4a7636487a40ff3d
Fabricio Cabral <fabriciofx@gmail.com>
2018-04-20 10:05:57 -0300

    Copyright year updated

--- src/main/java/com/github/fabriciofx/rps/match/SmartMatch.java
+++ src/main/java/com/github/fabriciofx/rps/match/SmartMatch.java
@@ -1,7 +1,7 @@
 /**
  * The MIT License (MIT)
  *
- * Copyright (C) 2017 Fabrício Barros Cabral
+ * Copyright (C) 2017-2018 Fabrício Barros Cabral
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
